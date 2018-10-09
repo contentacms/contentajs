3549067255a7ac6a3d95a3bd3875028abee34821
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2018-10-10 00:22:23 +0200

    feat: transition to a pm2-managed project

--- src/server.js
+++ src/server.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
 
-const { bootstrap } = require('./bootstrap');
-bootstrap();
+const _ = require('lodash');
+
+const fetchCmsMeta = require('./helpers/fetchCmsMeta');
+const startApp = require('./helpers/app');
+
+(async port => {
+  // Initialize JSON RPC.
+  const fetched = await fetchCmsMeta();
+  const mapped = {};
+  fetched.forEach(([map, jsonRpcResponse]) => {
+    Object.keys(map).forEach(variableName => {
+      const variableValue = _.get(jsonRpcResponse, [
+        'result',
+        ...map[variableName].split('.'),
+      ]);
+      mapped[variableName] = variableValue;
+    });
+  });
+  // Proxy for the JSON API server in Contenta CMS.
+  const app = startApp(mapped);
+  app.listen(port);
+})(process.env.PORT);
