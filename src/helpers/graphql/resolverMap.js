7667d54c0b41bce7e59028351a190842ae1c1c30
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2018-10-31 00:05:36 +0100

    docs: add code comment in the graphql resolver

--- src/helpers/graphql/resolverMap.js
+++ src/helpers/graphql/resolverMap.js
@@ -1,5 +1,10 @@
 module.exports = {
   Recipe: {
+    // This is one of the big wins of having GraphQL in node.js and not in PHP.
+    // Imagine that this field instead of generating a random value fetches
+    // reviews for the current recipe from a 3rd party site. You don't have to
+    // suffer a VERY HIGH performance degradation due to the HTTP request since
+    // node.js has non-blocking I/O. Drupal has not.
     random: (obj, args, context, info) => `${Math.random()}::${info.fieldName}`,
   },
 };
